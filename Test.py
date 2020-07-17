68fd0481751647b6314386fc05ef527b8f2afb72
PatrickGDaunt <patrickdaunt@icloud.com>
2020-07-16 22:29:34 -0400

    Initialize testing file @16 2229(Z-4) July 2020

---
+++ Test.py
@@ -0,0 +1,14 @@
+'''
+    Project name: Assignment 2
+    File name: Test.py
+    Author: Patrick D
+    DTG created: 16 2224(Z-4) July 2020
+    DTG last modification: 16 2230(Z-4) July 2020
+    Python Version: 3.7.4
+    Description: Test cases and unit tests for CharacterCreator.py
+    Classes:
+'''
+# Importing all the required packages
+import requests
+import unittest
+
